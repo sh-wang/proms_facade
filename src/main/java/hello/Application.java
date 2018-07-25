15240692fa3f9740f52b25f4832c6568ab799e66
Shihang Wang <ucabsw2@ucl.ac.uk>
2018-07-25 12:17:26 +0100

    first commit

---
+++ src/main/java/hello/Application.java
@@ -0,0 +1,40 @@
+package hello;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+public class Application {
+
+	private static final Logger log = LoggerFactory.getLogger(Application.class);
+
+	public static void main(String args[]) {
+		SpringApplication.run(Application.class);
+	}
+	
+	@Bean
+	public RestTemplate restTemplate(RestTemplateBuilder builder) {
+		return builder.build();
+	}
+
+	@Bean
+	public CommandLineRunner run(RestTemplate restTemplate) throws Exception {
+		return args -> {
+//			Quote quote = restTemplate.getForObject(
+//					"http://gturnquist-quoters.cfapps.io/api/random", Quote.class);
+//			log.info(quote.toString());
+			String fooResourceUrl
+					= "http://localhost:8080/api/fhir/Patient";
+			ResponseEntity<String> response
+					= restTemplate.getForEntity(fooResourceUrl + "/1", String.class);
+			System.out.println(response);
+		};
+	}
+}
